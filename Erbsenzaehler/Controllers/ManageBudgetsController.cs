77fbd1f255183f9cf578f2b77445ece291b128d7
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-03-03 06:15:17 +0100

    New controller to CRUD budgets.

---
+++ Erbsenzaehler/Controllers/ManageBudgetsController.cs
@@ -0,0 +1,119 @@
+﻿using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Erbsenzaehler.Models;
+using Erbsenzaehler.Rules;
+using Erbsenzaehler.ViewModels.Rules;
+using Newtonsoft.Json;
+
+namespace Erbsenzaehler.Controllers
+{
+    [Authorize]
+    public class ManageBudgetsController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        #region Json 
+
+        public async Task<ActionResult> Json()
+        {
+            var currentClient = await GetCurrentClient();
+            var rules = (await Db.Budgets
+                .Where(x => x.ClientId == currentClient.Id)
+                .OrderBy(x => x.Category)
+                .ToListAsync())
+                .Select(x => new JsonBudget(x));
+            return base.Json(rules, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Json(JsonBudget jsonBudget)
+        {
+            var currentClient = await GetCurrentClient();
+
+            var budget = new Budget
+            {
+                Category = jsonBudget.category,
+                Limit = jsonBudget.limit,
+                Period = jsonBudget.period,
+                ClientId = currentClient.Id
+            };
+            Db.Budgets.Add(budget);
+            await Db.SaveChangesAsync();
+
+            return base.Json(new JsonBudget(budget));
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> Json(int id, JsonBudget jsonBudget)
+        {
+            var currentClient = await GetCurrentClient();
+
+            var budget = await Db.Budgets.FirstOrDefaultAsync(x => x.ClientId == currentClient.Id && x.Id == id);
+            if (budget == null)
+            {
+                // if rule does not exist, create new one
+                return await Json(jsonBudget);
+            }
+
+            budget.Category = jsonBudget.category;
+            budget.Limit = jsonBudget.limit;
+            budget.Period = jsonBudget.period;
+            await Db.SaveChangesAsync();
+
+            return base.Json(new JsonBudget(budget));
+        }
+
+
+        [HttpDelete]
+        public async Task<ActionResult> Json(int id)
+        {
+            var currentClient = await GetCurrentClient();
+
+            var budget = await Db.Budgets.FirstOrDefaultAsync(x => x.ClientId == currentClient.Id && x.Id == id);
+            if (budget != null)
+            {
+                Db.Budgets.Remove(budget);
+                await Db.SaveChangesAsync();
+            }
+
+            return base.Json(new JsonBudget(budget));
+        }
+
+
+        public class JsonBudget
+        {
+            public JsonBudget()
+            {
+            }
+            
+         
+            public JsonBudget(Budget budget)
+            {
+                id = budget.Id;
+                category = budget.Category;
+                limit = budget.Limit;
+                period = budget.Period;
+            }
+            
+
+            // ReSharper disable InconsistentNaming
+            public int id { get; set; }
+            public string category { get; set; }
+            public decimal limit { get; set; }
+            public Budget.LimitPeriod period { get; set; }
+            // ReSharper restore InconsistentNaming
+        }
+
+        #endregion
+    }
+}
