30798b2196aa991e62d29730606bc483f78ac2de
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-04-02 22:12:50 +0200

    Added exception reporting to Airbrake.

--- Erbsenzaehler/App_Start/FilterConfig.cs
+++ Erbsenzaehler/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using SharpBrake;
 
 namespace Erbsenzaehler
 {
@@ -6,7 +7,20 @@ namespace Erbsenzaehler
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new SendErrorToAirbrakErrorAttribute());
+        }
+
+
+        public class SendErrorToAirbrakErrorAttribute : HandleErrorAttribute
+        {
+            public override void OnException(ExceptionContext filterContext)
+            {
+                if (!filterContext.HttpContext.Request.IsLocal)
+                {
+                    filterContext.Exception.SendToAirbrake();
+                }
+                base.OnException(filterContext);
+            }
         }
     }
 }
--- Erbsenzaehler/Global.asax.cs
+++ Erbsenzaehler/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System.Web;
+﻿using System.Threading;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SharpBrake;
 
 namespace Erbsenzaehler
 {
@@ -13,6 +15,19 @@ namespace Erbsenzaehler
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+            if (ex is ThreadAbortException)
+                return;
+
+            if (!Request.IsLocal)
+            {
+                ex.SendToAirbrake();
+            }
         }
     }
 }
