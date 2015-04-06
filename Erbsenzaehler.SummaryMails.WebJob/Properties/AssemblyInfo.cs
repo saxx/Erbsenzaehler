99cb16cb48baae57c6728d261a5b91e1c954d56a
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-04-06 15:07:25 +0200

    Add WebJob for sending summary mails.

---
+++ Erbsenzaehler.SummaryMails.WebJob/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("WebJob for sending Erbsenzähler summary mails")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Erbsenzähler")]
+[assembly: AssemblyCopyright("Copyright © Hannes Sachsenhofer 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
