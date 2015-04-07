e6814703383d266c4e6bae1c82ae5ea6f74738b1
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-04-07 17:59:11 +0200

    Added missing file to repo.

---
+++ Erbsenzaehler.Common/Templates/RazorTemplateResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using RazorEngine.Templating;
+
+#pragma warning disable 618
+namespace Erbsenzaehler.Templates
+{
+    public class RazorTemplateResolver : ITemplateResolver
+    {
+        public string Resolve(string templateName)
+        {
+            if (templateName == null)
+            {
+                throw new ArgumentNullException("templateName");
+            }
+
+            return File.ReadAllText(GetTemplatePath(templateName));
+        }
+
+        private string GetTemplatePath(string templateName)
+        {
+            if (templateName == null)
+            {
+                throw new ArgumentNullException("templateName");
+            }
+
+            var templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Templates", templateName);
+            if (!File.Exists(templatePath))
+            {
+                templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin", "Templates", templateName);
+            }
+
+            if (!File.Exists(templatePath))
+            {
+                throw new Exception("Template '" + templateName + "' missing. It should be at '" + templatePath + "'.");
+            }
+
+            return templatePath;
+        }
+    }
+}
+#pragma warning restore 618
