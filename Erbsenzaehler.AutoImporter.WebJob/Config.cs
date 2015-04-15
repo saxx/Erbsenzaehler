17c5ccd1d8d4b397302103784718c79c5b08bd98
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-04-15 18:51:07 +0200

    Added web job project for auto importer.

---
+++ Erbsenzaehler.AutoImporter.WebJob/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace Erbsenzaehler.AutoImporter.WebJob
+{
+    public static class Config
+    {
+        public static string ErbsenzaehlerUrl => ConfigurationManager.AppSettings["Erbsenzaehler.Url"] ?? "http://erbsenzaehler.azurewebsites.net";
+        public static string OneTrueErrorAppKey => ConfigurationManager.AppSettings["OneTrueError.AppKey"] ?? "";
+        public static string OneTrueErrorAppSecret => ConfigurationManager.AppSettings["OneTrueError.AppSecret"] ?? "";
+    }
+}
